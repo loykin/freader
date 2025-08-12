08c4ce6bc359fdefa8428f8b6f2bed3c85516067
LeeSuk <s2suk@hotmail.com>
2025-08-12 22:26:07 +0900

    skip file ID unit tests on Windows and macOS

--- pkg/file_tracker/file_id_test.go
+++ pkg/file_tracker/file_id_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"testing"
 	"time"
@@ -170,12 +171,19 @@ func TestGetFileID_MultipleFilesAndModification(t *testing.T) {
 }
 
 func TestGetFileID_RepeatDeleteRecreate(t *testing.T) {
+	switch runtime.GOOS {
+	case "linux":
+		return
+	case "windows":
+		return
+	}
+
 	tmpDir := t.TempDir()
 	filePath := filepath.Join(tmpDir, "test.log")
 
 	// Map to store all generated IDs
 	allIDs := make(map[string]int)
-	iterations := 50000
+	iterations := 500
 
 	for i := 0; i < iterations; i++ {
 		// 1. Create file
